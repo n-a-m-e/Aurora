a63f908e97261e70e2043116e29851454287153e
n-a-m-e <n-a-m-e@users.noreply.github.com>
2024-11-20 18:18:22 +1100

    Update

--- files/scripts/systemd/node-server.sh
+++ files/scripts/systemd/node-server.sh
@@ -9,4 +9,4 @@ setfacl -b -R /home/Node
 chown -R root:users /home/Node
 chmod -R 2775 /home/Node
 
-docker run -it --rm -p 8080:8080 -v "/home/Node:/home/Node" -v "/home/Shared:/home/Shared" -w "/home/Node" node:lts-slim bash -c "cd ..; node Node/server.js"
+docker run --rm -p 8000:8000 -v "/home/Node:/home/Node" -v "/home/Shared:/home/Shared" -w "/home/Node" node:lts-slim bash -c "cd ..; node Node/server.js"
