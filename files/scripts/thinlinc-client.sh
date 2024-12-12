ba2f9547d0eab92ddd8c3c70acd475ae4a58205b
n-a-m-e <n-a-m-e@users.noreply.github.com>
2024-12-12 15:46:38 +1100

    Update

--- files/scripts/thinlinc-client.sh
+++ files/scripts/thinlinc-client.sh
@@ -10,3 +10,17 @@ mkdir /tmp/thinlinc-client
 wget -O /tmp/thinlinc-client/thinlinc-client-4.17.0-3543.x86_64.rpm https://github.com/n-a-m-e/Aurora-Files/releases/download/thinlinc-client-4.17.0-3543/thinlinc-client-4.17.0-3543.x86_64.rpm
 cd /tmp/thinlinc-client
 sudo rpm-ostree install /tmp/thinlinc-client/thinlinc-client*.rpm
+
+#/opt does not persist after build so move to /usr/lib/opt
+mv /opt/thinlinc /usr/lib/opt/thinlinc
+
+#create required directories and symlinks at boot
+cat <<'EOF' > /usr/lib/tmpfiles.d/thinlinc-client.conf
+d /var/opt/thinlinc 755 root root -
+L+ /var/opt/thinlinc/lib - - - - /usr/lib/opt/thinlinc/lib
+L+ /var/opt/thinlinc/etc - - - - /usr/lib/opt/thinlinc/etc
+L+ /var/opt/thinlinc/bin - - - - /usr/lib/opt/thinlinc/bin
+EOF
+
+mkdir -p /usr/etc/xdg/autostart
+cp /usr/share/applications/thinlinc-client.desktop /usr/etc/xdg/autostart/thinlinc-client.desktop
