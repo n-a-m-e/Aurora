22bbcaeab5809473740857f9805b372994ac0bb7
n-a-m-e <n-a-m-e@users.noreply.github.com>
2024-12-15 11:03:24 +1100

    Create systemd-client.sh

---
+++ files/scripts/systemd-client.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+cd "$(dirname "$0")"
+cp systemd/hostname-reset-for-client.service /usr/lib/systemd/system/hostname-reset-for-client.service
