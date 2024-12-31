156963011f3b99a1610b3547fad3ff556da0c333
n-a-m-e <n-a-m-e@users.noreply.github.com>
2024-12-31 14:18:15 +1100

    Update vikunja.sh

--- files/scripts/vikunja.sh
+++ files/scripts/vikunja.sh
@@ -11,12 +11,22 @@ wget -O /tmp/vikunja/vikunja-0.24.6-x86_64.rpm https://github.com/n-a-m-e/Aurora
 cd /tmp/vikunja
 sudo rpm-ostree install /tmp/vikunja/vikunja*.rpm
 
+#configure options in /etc/vikunja/config.yml
+sed -i 's|timezone:.*|timezone: AEST|g' /etc/vikunja/config.yml
+sed -i 's|week_start: 0|week_start: 1|g' /etc/vikunja/config.yml
+sed -i 's|language: <unset>|language: en|g' /etc/vikunja/config.yml
+sed -i 's|path: "./vikunja.db"|path: "/var/opt/vikunja/vikunja.db"|g' /etc/vikunja/config.yml
+sed -i 's|basepath: ./files|basepath: /var/opt/vikunja/files|g' /etc/vikunja/config.yml
+sed -i 's|path: /opt/vikunja/logs|path: /var/opt/vikunja/logs|g' /etc/vikunja/config.yml
+
 #/opt does not persist after build so move to /usr/lib/opt
 mv /opt/vikunja /usr/lib/opt/vikunja
 
 #create required directories and symlinks at boot
 cat <<'EOF' > /usr/lib/tmpfiles.d/vikunja.conf
 d /var/opt/vikunja 755 root root -
+d /var/opt/vikunja/files 755 root root -
+d /var/opt/vikunja/logs 755 root root -
 L+ /var/opt/vikunja/vikunja - - - - /usr/lib/opt/vikunja/vikunja
 L+ /usr/bin/vikunja - - - - /var/opt/vikunja/vikunja
 EOF
