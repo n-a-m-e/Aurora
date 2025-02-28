32ebe9f7b932de50684a864e626af926eae10872
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-02-28 18:24:32 +1100

    Update olive.sh

--- files/scripts/olive.sh
+++ files/scripts/olive.sh
@@ -5,13 +5,17 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-mkdir /tmp/olive
-#wget -O /tmp/olive/Olive-1e3cf53-x86_64.AppImage https://github.com/olive-editor/olive/releases/download/0.1.0/Olive-1e3cf53-x86_64.AppImage
-wget -O /tmp/olive/Olive-1e3cf53-x86_64.AppImage https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64/Olive-1e3cf53-x86_64.AppImage
-wget -O /tmp/olive/Olive-1e3cf53-x86_64-share.zip https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64-share/Olive-1e3cf53-x86_64-share.zip
-cd /tmp/olive
-unzip Olive-1e3cf53-x86_64-share.zip
-cp -r /tmp/olive/share /usr/share
 mkdir /usr/lib/opt/olive
-mv /tmp/olive/Olive-1e3cf53-x86_64.AppImage /usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage
+wget -O /usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64/Olive-1e3cf53-x86_64.AppImage
 chmod a+x "/usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage"
+
+wget -O /usr/Olive-1e3cf53-x86_64-share.zip https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64-share/Olive-1e3cf53-x86_64-share.zip
+cd /usr
+unzip Olive-1e3cf53-x86_64-share.zip
+rm -f /usr/Olive-1e3cf53-x86_64-share.zip
+
+#create required directories and symlinks at boot
+cat <<'EOF' > /usr/lib/tmpfiles.d/olive.conf
+d /var/opt/olive 755 root root -
+L+ /var/opt/olive/Olive-1e3cf53-x86_64.AppImage - - - - /usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage
+EOF
