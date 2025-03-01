1969156701fa6e88c0b71190e0672d6d5c599496
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-01 16:46:14 +1100

    Update olive.sh

--- files/scripts/olive.sh
+++ files/scripts/olive.sh
@@ -7,12 +7,15 @@ set -oue pipefail
 
 mkdir /usr/lib/opt/olive
 wget -O /usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64/Olive-1e3cf53-x86_64.AppImage
+wget -O /usr/lib/opt/olive/Olive-8ac191ce-Linux-x86_64.AppImage https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-8ac191ce-Linux-x86_64/Olive-8ac191ce-Linux-x86_64.AppImage
 chmod a+x "/usr/lib/opt/olive/Olive-1e3cf53-x86_64.AppImage"
+chmod a+x "/usr/lib/opt/olive/Olive-8ac191ce-Linux-x86_64.AppImage"
 
-wget -O /usr/Olive-1e3cf53-x86_64-share.zip https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-1e3cf53-x86_64-share/Olive-1e3cf53-x86_64-share.zip
+wget -O /usr/Olive-1e3cf53-x86_64-share.zip https://github.com/n-a-m-e/Aurora-Files/releases/download/Olive-Share-x86_64/Olive-Share-x86_64.zip
 cd /usr
 unzip Olive-1e3cf53-x86_64-share.zip
 rm -f /usr/Olive-1e3cf53-x86_64-share.zip
+update-mime-database /usr/share/mime
 
 #create required directories and symlinks at boot
 cat <<'EOF' > /usr/lib/tmpfiles.d/olive.conf
