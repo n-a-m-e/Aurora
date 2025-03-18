a2b82de1d945cfca9801021df3810f2ea96e433e
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-18 13:26:18 +1100

    Create nix.sh

---
+++ files/scripts/nix.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate
+nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
+
+#auto.nixGLDefault: Tries to auto-detect and install Nvidia, if not, fallback to mesa. Recommended. Invoke with nixGL program.
+#auto.nixGLNvidia: Proprietary Nvidia driver (auto detection of version)
+#auto.nixGLNvidiaBumblebee: Proprietary Nvidia driver on hybrid hardware (auto detection).
+#nixGLIntel: Mesa OpenGL implementation (intel, amd, nouveau, ...).
+#auto.nixVulkanNvidia: Proprietary Nvidia driver (auto detection).
+#nixVulkanIntel: Mesa Vulkan implementation.
+
+nix-env -iA nixgl.nixGLIntel   # or replace `nixGLDefault` with your desired wrapper
+nix-env -iA nixos.davinci-resolvenix-env -iA nixos.davinci-resolve
