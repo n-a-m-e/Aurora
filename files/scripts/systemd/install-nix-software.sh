09f45a8a73668899a6cfcf3741b8b08059a11c61
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-20 10:08:32 +1100

    Update install-nix-software.sh

--- files/scripts/systemd/install-nix-software.sh
+++ files/scripts/systemd/install-nix-software.sh
@@ -10,6 +10,7 @@ else
   chmod a+x "/tmp/nix/nix-installer-x86_64-linux"
   cd /tmp/nix
   ./nix-installer-x86_64-linux install ostree --determinate --no-confirm
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
 if command -v nixGL >&2; then
@@ -17,7 +18,7 @@ if command -v nixGL >&2; then
 else
   echo nixGL is not available
   while ! ping -c1 github.com; do sleep 2; done
-  /bin/bash -c 'nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update'
+  nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
   #auto.nixGLDefault: Tries to auto-detect and install Nvidia, if not, fallback to mesa. Recommended. Invoke with nixGL program.
   #auto.nixGLNvidia: Proprietary Nvidia driver (auto detection of version)
   #auto.nixGLNvidiaBumblebee: Proprietary Nvidia driver on hybrid hardware (auto detection).
@@ -25,7 +26,7 @@ else
   #auto.nixVulkanNvidia: Proprietary Nvidia driver (auto detection).
   #nixVulkanIntel: Mesa Vulkan implementation.
   #nix-env -iA nixgl.nixGLIntel or replace `nixGLDefault` with your desired wrapper
-  /bin/bash -c 'nix-env -iA nixgl.nixGLIntel'
+  nix-env -iA nixgl.nixGLIntel
 fi
 
 if command -v davinci-resolve >&2; then
@@ -33,5 +34,5 @@ if command -v davinci-resolve >&2; then
 else
   echo davinci-resolve is not available
   while ! ping -c1 github.com; do sleep 2; done
-  /bin/bash -c 'nix-env -iA nixos.davinci-resolvenix-env -iA nixos.davinci-resolve'
+  nix-env -iA nixos.davinci-resolvenix-env -iA nixos.davinci-resolve
 fi
