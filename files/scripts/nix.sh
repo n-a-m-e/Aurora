0e4cd8a7c2c69ead03b50ba91452981cef43fdfe
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-19 14:36:22 +1100

    Update nix.sh

--- files/scripts/nix.sh
+++ files/scripts/nix.sh
@@ -5,27 +5,12 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-mkdir /tmp/nix
-wget -O /tmp/nix/nix-multi-user-2.24.10.rpm https://nix-community.github.io/nix-installers/nix/x86_64/nix-multi-user-2.24.10.rpm
-cd /tmp/nix
-rpm-ostree install /tmp/nix/nix-multi-user*.rpm
-
-nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
-
-#auto.nixGLDefault: Tries to auto-detect and install Nvidia, if not, fallback to mesa. Recommended. Invoke with nixGL program.
-#auto.nixGLNvidia: Proprietary Nvidia driver (auto detection of version)
-#auto.nixGLNvidiaBumblebee: Proprietary Nvidia driver on hybrid hardware (auto detection).
-#nixGLIntel: Mesa OpenGL implementation (intel, amd, nouveau, ...).
-#auto.nixVulkanNvidia: Proprietary Nvidia driver (auto detection).
-#nixVulkanIntel: Mesa Vulkan implementation.
-
-nix-env -iA nixgl.nixGLIntel   # or replace `nixGLDefault` with your desired wrapper
-nix-env -iA nixos.davinci-resolvenix-env -iA nixos.davinci-resolve
-
-#/nix does not persist after build so move to /usr/nix
-mv /nix /usr/nix
-
-#create required directories and symlinks at boot
-cat <<'EOF' > /usr/lib/tmpfiles.d/nix.conf
-L+ /nix - - - - /usr/nix
+#Transient root needs to be enabled to mount /nix with composefs enabled
+cat <<'EOF' >> /usr/lib/ostree/prepare-root.conf
+[root]
+transient = true
 EOF
+
+cp systemd/install-nix-software.service /usr/lib/systemd/system/install-nix-software.service
+cp systemd/install-nix-software.sh /usr/sbin/install-nix-software.sh
+chmod a+x "/usr/sbin/install-nix-software.sh"
