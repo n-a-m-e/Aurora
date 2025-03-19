225af8a0022ad45ce15b952bca1515d933f0d853
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-19 14:32:51 +1100

    Create install-nix-software.sh

---
+++ files/scripts/systemd/install-nix-software.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if command -v nix >&2; then
+  echo nix is available
+else
+  echo nix is not available
+  mkdir /tmp/nix
+  wget -O /tmp/nix/nix-installer-x86_64-linux https://install.determinate.systems/nix/nix-installer-x86_64-linux
+  chmod a+x "/tmp/nix/nix-installer-x86_64-linux"
+  cd /tmp/nix
+  ./nix-installer-x86_64-linux install ostree --determinate --no-confirm
+  nix-channel --add https://github.com/nix-community/nixGL/archive/main.tar.gz nixgl && nix-channel --update
+  #auto.nixGLDefault: Tries to auto-detect and install Nvidia, if not, fallback to mesa. Recommended. Invoke with nixGL program.
+  #auto.nixGLNvidia: Proprietary Nvidia driver (auto detection of version)
+  #auto.nixGLNvidiaBumblebee: Proprietary Nvidia driver on hybrid hardware (auto detection).
+  #nixGLIntel: Mesa OpenGL implementation (intel, amd, nouveau, ...).
+  #auto.nixVulkanNvidia: Proprietary Nvidia driver (auto detection).
+  #nixVulkanIntel: Mesa Vulkan implementation.
+  #nix-env -iA nixgl.nixGLIntel or replace `nixGLDefault` with your desired wrapper
+  nix-env -iA nixgl.nixGLIntel -iA nixos.davinci-resolvenix-env -iA nixos.davinci-resolve
+fi
