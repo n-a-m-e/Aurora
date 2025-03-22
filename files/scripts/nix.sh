c50c419770501638a65490961f8bd0528b808cbd
n-a-m-e <n-a-m-e@users.noreply.github.com>
2025-03-22 11:04:11 +1100

    Update nix.sh

--- files/scripts/nix.sh
+++ files/scripts/nix.sh
@@ -11,11 +11,6 @@ cat <<'EOF' >> /usr/lib/ostree/prepare-root.conf
 transient = true
 EOF
 
-#create required directories and symlinks at boot
-cat <<'EOF' > /usr/lib/tmpfiles.d/nix-opengl-driver.conf
-L+ /run/opengl-driver - - - - /home/nix/opengl-driver
-EOF
-
 cp systemd/install-nix-software.service /usr/lib/systemd/system/install-nix-software.service
 cp systemd/install-nix-software.sh /usr/sbin/install-nix-software.sh
 chmod a+x "/usr/sbin/install-nix-software.sh"
