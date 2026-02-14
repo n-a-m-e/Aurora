108f4afb253e27129dfc5a32b3dae101dfe2bc78
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-02-14 19:26:18 +1100

    Update systemd-client.sh

--- files/scripts/systemd-client.sh
+++ files/scripts/systemd-client.sh
@@ -7,4 +7,7 @@ set -oue pipefail
 
 cd "$(dirname "$0")"
 cp systemd/hostname-reset-for-client.service /usr/lib/systemd/system/hostname-reset-for-client.service
+
 cp systemd/kscreenlocker-fprint-login.service /usr/lib/systemd/system/kscreenlocker-fprint-login.service
+cp systemd/kscreenlocker-fprint-login.sh /usr/sbin/kscreenlocker-fprint-login.sh
+chmod a+x "/usr/sbin/kscreenlocker-fprint-login.sh"
