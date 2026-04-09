6a96c88ffc4a04a7a38d207f85126c65f24e1152
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-04-09 18:13:50 +1000

    Create sunshine.sh

---
+++ files/scripts/sunshine.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+dnf -y copr enable lizardbyte/beta
+rpm-ostree install Sunshine libcap
+
+SUNSHINE_BIN="$(readlink -f "$(command -v sunshine)")"
+if [[ -n "${SUNSHINE_BIN:-}" ]]; then
+  setcap -r "$SUNSHINE_BIN" || true
+fi
