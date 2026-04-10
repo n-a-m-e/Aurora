90519dab9b1852693c0d383e1e593219fad41893
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-04-10 11:10:38 +1000

    Update davinci-resolve.sh

--- files/scripts/davinci-resolve.sh
+++ files/scripts/davinci-resolve.sh
@@ -6,5 +6,4 @@ rpm-ostree install apr apr-util libxcrypt-compat mesa-libGLU rocminfo rocm-openc
 cat <<'EOF' > /usr/lib/tmpfiles.d/davinci-resolve.conf
 d /var/opt/resolve 0755 root root -
 d /var/opt/resolve-installer 0755 root root -
-L+ /opt/resolve - - - - /var/opt/resolve
 EOF
