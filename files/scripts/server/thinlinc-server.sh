94c968aeb64a4a9677521c237f4f1a93f34bf1d9
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-04-23 14:05:41 +1000

    Update thinlinc-server.sh

--- files/scripts/server/thinlinc-server.sh
+++ files/scripts/server/thinlinc-server.sh
@@ -3,6 +3,7 @@ set -Eeuo pipefail
 source /usr/lib/bluebuild-debug.sh
 
 mkdir /tmp/thinlinc
+trap 'rm -rf /tmp/thinlinc' EXIT
 #wget -O /tmp/thinlinc/tl-4.17.0-server.zip https://www.cendio.com/downloads/server/tl-4.17.0-server.zip
 wget -O /tmp/thinlinc/tl-4.17.0-server.zip https://github.com/n-a-m-e/Aurora-Files/releases/download/tl-4.17.0-server/tl-4.17.0-server.zip
 cd /tmp/thinlinc
