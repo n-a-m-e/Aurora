95a3881071073976943dc6ce7df2b4b47634d2bf
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-04-23 13:56:19 +1000

    Update fingerprint.sh

--- files/scripts/client/fingerprint.sh
+++ files/scripts/client/fingerprint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
+set -Eeuo pipefail
+source /usr/lib/bluebuild-debug.sh
 
 #for login prepend fingerprint reader to /usr/etc/pam.d/sddm
 mkdir -p /usr/etc/pam.d
