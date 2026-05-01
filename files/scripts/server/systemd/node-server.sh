a0a67c336587279c5afd5fc1e6b986928e9d6c9d
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-05-01 10:29:58 +1000

    Update node-server.sh

--- files/scripts/server/systemd/node-server.sh
+++ files/scripts/server/systemd/node-server.sh
@@ -10,6 +10,6 @@ chown -R root:users /home/Node
 chmod -R 2775 /home/Node
 
 # Install packages from /home/Node/package.json inside the same Node image.
-docker run --rm -v "/home/Node:/home/Node" -w "/home/Node" node:lts-slim npm install
+docker run --rm -v "/home/Node:/home/Node" -w "/home/Node" -e GECKODRIVER_AUTO_INSTALL=1 node:lts-slim npm install
   
 docker run --rm --network=host -v "/home/Node:/home/Node" -v "/home/Shared:/home/Shared" -w "/home/Node" node:lts-slim bash -c "cd ..; node Node/server.js"
