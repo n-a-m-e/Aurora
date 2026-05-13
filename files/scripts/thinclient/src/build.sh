399726a53238213d634128d6b5803e3ada1156c6
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-05-13 13:34:44 +1000

    Update build.sh

--- files/scripts/thinclient/src/build.sh
+++ files/scripts/thinclient/src/build.sh
@@ -1,56 +1,35 @@
 #!/usr/bin/bash
+
 set -exo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Create the directory that /root is symlinked to
+mkdir -p "$(realpath /root)"
 
-dnf install -qy \
-    anaconda \
-    anaconda-install-img-deps \
-    anaconda-dracut \
-    dracut-config-generic \
-    dracut-network \
-    net-tools \
-    grub2-efi-x64-cdboot
-
-# these are necessary build tools, if you have a separate build container
-# in `--bootc-build-ref` then these can go there
-dnf install -qy \
-    xorrisofs \
-    squashfs-tools
-
-dnf clean all
-
-mkdir -p /boot/efi \
-    && cp -ra /usr/lib/efi/*/*/EFI /boot/efi
-
-# this stuff is normally done by lorax (runtime-postinstall.tmpl), i've distilled
-# it down to the bare bones necessary
-echo "install:x:0:0:root:/root:/usr/libexec/anaconda/run-anaconda" >> /etc/passwd
-echo "install::14438:0:99999:7:::" >> /etc/shadow
-passwd -d root
-
-mv /usr/share/anaconda/list-harddrives-stub /usr/bin/list-harddrives
-mv /etc/yum.repos.d /etc/anaconda.repos.d
-ln -s /lib/systemd/system/anaconda.target /etc/systemd/system/default.target
-rm -v /usr/lib/systemd/system-generators/systemd-gpt-auto-generator
-
-rm /usr/lib/systemd/system/autovt@.service
-ln -s /usr/lib/systemd/system/anaconda-shell@.service /usr/lib/systemd/system/autovt@.service
-
-mkdir /usr/lib/systemd/logind.conf.d
-echo "[Login]\nReserveVT=2" > /usr/lib/systemd/logind.conf.d/anaconda-shell.conf
-
-# regenerate the initramfs including the anaconda module
-mkdir "$(realpath /root)"
+# Install dracut-live and regenerate the initramfs
+dnf install -y dracut-live
 kernel=$(kernel-install list --json pretty | jq -r '.[] | select(.has_kernel == true) | .version')
 DRACUT_NO_XATTR=1 dracut -v --force --zstd --reproducible --no-hostonly \
-  --add "anaconda" \
+    --add "dmsquash-live dmsquash-live-autooverlay" \
     "/usr/lib/modules/${kernel}/initramfs.img" "${kernel}"
 
-# set the defaults for anaconda, this includes the container that will be
-# installed onto the system
-cp /src/interactive-defaults.ks /usr/share/anaconda/interactive-defaults.ks
+# Install livesys-scripts and configure them
+dnf install -y livesys-scripts
+sed -i "s/^livesys_session=.*/livesys_session=gnome/" /etc/sysconfig/livesys
+systemctl enable livesys.service livesys-late.service
 
-# set the defaults for (bootc-)image-builder
-mkdir /usr/lib/bootc-image-builder
+# image-builder needs gcdx64.efi
+dnf install -y grub2-efi-x64-cdboot
+
+# image-builder expects the EFI directory to be in /boot/efi
+mkdir -p /boot/efi
+cp -av /usr/lib/efi/*/*/EFI /boot/efi/
+
+# needed for image-builder's buildroot
+dnf install -y xorriso isomd5sum squashfs-tools
+
+# Copy in the iso config for image-builder
+mkdir -p /usr/lib/bootc-image-builder
 cp /src/iso.yaml /usr/lib/bootc-image-builder/iso.yaml
+
+# Clean up dnf cache to save space
+dnf clean all
