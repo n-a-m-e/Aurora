56cd56b9784286b81b7a1e5f5da6f07284b70ebc
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-05-13 15:28:35 +1000

    Update konsole.sh

--- files/scripts/shared/konsole.sh
+++ files/scripts/shared/konsole.sh
@@ -8,10 +8,10 @@ set -oue pipefail
 #Override Aurora Changes https://github.com/ublue-os/bluefin/blob/de4cec23b0328857f99bebeb2874679bc23c85d7/build_files/aurora-changes.sh
 #sed -i 's|org.gnome.Ptyxis.desktop|org.kde.konsole.desktop|g' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 #sed -i 's|org.gnome.Ptyxis.desktop|org.kde.konsole.desktop|g' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
-sed -i 's|X-KDE-Shortcuts=Ctrl+Alt+T||g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's|Keywords=konsole;console;|Keywords=|g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's|\[Desktop Entry\]|\[Desktop Entry\]\nNoDisplay=true|g' /usr/share/applications/org.gnome.Ptyxis.desktop
-rm -f /usr/share/kglobalaccel/org.gnome.Ptyxis.desktop
+#sed -i 's|X-KDE-Shortcuts=Ctrl+Alt+T||g' /usr/share/applications/org.gnome.Ptyxis.desktop
+#sed -i 's|Keywords=konsole;console;|Keywords=|g' /usr/share/applications/org.gnome.Ptyxis.desktop
+#sed -i 's|\[Desktop Entry\]|\[Desktop Entry\]\nNoDisplay=true|g' /usr/share/applications/org.gnome.Ptyxis.desktop
+#rm -f /usr/share/kglobalaccel/org.gnome.Ptyxis.desktop
 sed -i 's|NoDisplay=true||g' /usr/share/applications/org.kde.konsole.desktop
 cp /usr/share/applications/org.kde.konsole.desktop /usr/share/kglobalaccel/org.kde.konsole.desktop
 
