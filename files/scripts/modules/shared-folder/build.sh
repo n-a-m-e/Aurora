a31180db048f17092babbf90ae38a3cb622f1132
n-a-m-e <n-a-m-e@users.noreply.github.com>
2026-06-04 09:19:25 +1000

    Update build.sh

--- files/scripts/modules/shared-folder/build.sh
+++ files/scripts/modules/shared-folder/build.sh
@@ -5,6 +5,8 @@ source /usr/lib/bluebuild-debug.sh
 chmod a+x "/usr/bin/shared-folder.sh"
 chmod a+x "/usr/bin/node-server.sh"
 
+sed -i 's|SocketGroup=docker|SocketGroup=users|' /usr/lib/systemd/system/docker.socket
+
 #Change Umask to make shared folders possible
 sed -i 's/UMASK		022/UMASK		002/g' /etc/login.defs
 sed -i 's/HOME_MODE	0700/HOME_MODE	0770/g' /etc/login.defs
